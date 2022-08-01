8731a5d6bb87943b43e7477c94e6815bd99a2502
Bruno Laguna <bruno_cs1.8@hotmail.com>
2022-08-01 10:52:43 -0300

    1° commit - branch 1
    - New style for the index
    - Changes in the function for creating files, loop for the extension with the number of users, each line of the text area for each index of the array.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function clicked() {
+async function clicked() {
     let division = document.getElementById('division').value
     // SELECT
     if (division == 0) {
@@ -10,13 +10,45 @@ function clicked() {
     } else {
         console.log('3')
     }
-    // Numeros usuarios
+    // Numeros de usuarios
     let user = document.getElementById('user').value
-    console.log(user)
+    console.log(Number.parseInt(user))
     // Extension
     let ext = document.querySelector('.extension').value
-    console.log(ext)
-    // Input nomes e logins
-    let nl = document.querySelector('textarea').value.toUpperCase()
-    console.log(nl)
+    let qtdExt = (Number.parseInt(ext)) + Number(user)
+    console.log(Number.parseInt(ext))
+    i = ext
+    for(i == ext; i < qtdExt; i++) {
+        console.log(i)
+    }
+    // Input nomes
+    let nome = document.querySelector('textarea').value.split('\n')
+    console.log(nome[1])
+    console.log(nome)
+    nome = nome.filter(i => i.trim())
+    console.log(nome)
+    if (nome.length != user) {
+        alert('Você colocou mais nomes na lista ou informou a quantidade de usuários errada!')
+    }
+    i=0
+    while(i <= user){
+    i++         
+    var myBlob = new Blob(
+            [`"${nome[i]}","jbrocanell@brasilseg.com.br","","46074","ATENDENTE","SUPV RELAC COM CLIENTES I","tnelima@brasilseg.com.br","SP","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""\n`],
+            {type: "text/plain"}
+            )
+    }
+    // criando um blob object
+
+    // File Handler $ STREAM
+    const fileHandle = await window.showSaveFilePicker()
+    const fileStream = await fileHandle.createWritable()
+
+    // SAVE FILE
+    await fileStream.write(myBlob)
+    await fileStream.close()
+
+    for(nome[i] == 0; nome[i] <= nome.length; nome[i]++){
+        
+    }
 }
