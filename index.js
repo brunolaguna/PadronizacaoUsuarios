5e2913f3401f0a1adb64f8a73e717b15a3c19067
Bruno Laguna <bruno_cs1.8@hotmail.com>
2022-08-01 11:39:44 -0300

    2° commit - branch 1
    - Improving the index style.
    - Improving the select input (hidden value null).
    - Organizing the HTML.
    - Organizing the JS and test it, working normally and correctly.

--- index.js
+++ index.js
@@ -1,54 +1,85 @@
 async function clicked() {
-    let division = document.getElementById('division').value
-    // SELECT
-    if (division == 0) {
-        console.log('0')
-    } else if (division == 1) {
-        console.log('1')    
-    } else if (division == 2) {
-        console.log('2')
-    } else {
-        console.log('3')
-    }
     // Numeros de usuarios
-    let user = document.getElementById('user').value
+    var user = document.getElementById('user').value
     console.log(Number.parseInt(user))
+
     // Extension
     let ext = document.querySelector('.extension').value
     let qtdExt = (Number.parseInt(ext)) + Number(user)
     console.log(Number.parseInt(ext))
-    i = ext
-    for(i == ext; i < qtdExt; i++) {
-        console.log(i)
-    }
-    // Input nomes
-    let nome = document.querySelector('textarea').value.split('\n')
-    console.log(nome[1])
-    console.log(nome)
-    nome = nome.filter(i => i.trim())
-    console.log(nome)
-    if (nome.length != user) {
-        alert('Você colocou mais nomes na lista ou informou a quantidade de usuários errada!')
-    }
-    i=0
-    while(i <= user){
-    i++         
-    var myBlob = new Blob(
-            [`"${nome[i]}","jbrocanell@brasilseg.com.br","","46074","ATENDENTE","SUPV RELAC COM CLIENTES I","tnelima@brasilseg.com.br","SP","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""\n`],
-            {type: "text/plain"}
-            )
-    }
-    // criando um blob object
+    e = ext
 
-    // File Handler $ STREAM
-    const fileHandle = await window.showSaveFilePicker()
-    const fileStream = await fileHandle.createWritable()
+    // email manager
+    let emailManager = document.getElementById('emailManager').value.toLowerCase()
 
-    // SAVE FILE
-    await fileStream.write(myBlob)
-    await fileStream.close()
+    // location
+    let location = document.getElementById('location').value
 
-    for(nome[i] == 0; nome[i] <= nome.length; nome[i]++){
+    //for(e == ext; e < qtdExt; e++) {
+        //    console.log(e)
+        //}
+        // Input nomes
+
+        // Names for each var
+        let nome = document.querySelector('#name_id').value.toUpperCase().split('\n')
+        console.log(nome[1])
+        console.log(nome)
+
+        nome = nome.filter(i => i.trim())
+        console.log(nome)
+
+        let login = document.getElementById('login_id').value.toLowerCase().split('\n')
+
+        login = login.filter(z => z.trim())
+        console.log(login)
+
+        i=0
+        var z=0
+        var hole = []
+        let string = `name,email,phone_work,extension,title,department,email_manager,location_work,roles_divisions,queues,division,skills,hire_date`
+        let division = document.getElementById('division').value
+        if (nome.length != user || nome.length === 0 || division == 0 || ext == "") {
+            alert('Você informou a quantidade de usuários errada, ou não informou nenhum usuário, ou não selecionou Division!')
+        } else {
+            
+            if (division == 1) {
+                console.log('1')
+                while(i<user){
+                    hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""`
+            }
+            } else if (division == 2) {
+                console.log('2')
+                while(i<user){
+                    hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","GAO","",""`
+            }    
+            } else if (division == 3) {
+                console.log('3')
+                while(i<user){
+                    hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Ouvidoria","",""`
+            }
+            } else {
+                console.log('4')
+                while(i<user){
+                    hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Tempo Assistência","",""`
+            }
+            }
+            var myBlob = new Blob(
+                [string, hole.join("")],
+                {type: "text/plain"}
+                )
+                
+                // File Handler $ STREAM
+                const fileHandle = await window.showSaveFilePicker()
+                const fileStream = await fileHandle.createWritable()
+                
+                // SAVE FILE
+                await fileStream.write(myBlob)
+                await fileStream.close()
+        }
         
-    }
-}
+        // SELECT
+    
+        //var l = `"${nome[i++]}","jbrocanell@brasilseg.com.br","","46074","ATENDENTE","SUPV RELAC COM CLIENTES I","tnelima@brasilseg.com.br","SP","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""\n`
+                
+        // criando um blob object
+        }
