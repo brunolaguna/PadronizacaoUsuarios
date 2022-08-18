d6615edc136838adefd42f8ae1e52d1a67af7665
Bruno Laguna <bruno_cs1.8@hotmail.com>
2022-08-18 16:03:59 -0300

    13° commit - branch1
    - Added the e-mail domain complement on the front, if the user does not define it.
    - Improved style.
    - Some unused commands were removed, to shorten the code.
    - Extension is no longer a required field.
    - The user field and variable was removed, as there was no need.
    - And the #back was finished.

--- index.js
+++ index.js
@@ -1,120 +1,219 @@
-async function clicked() {
-    // Numeros de usuarios
-    var user = document.getElementById('user').value
-    console.log(Number.parseInt(user))
+// User creation
+async function clicked(){ //This function must be asynchronous, otherwise the file will not save the data.
 
     // Extension
-    let ext = document.querySelector('.extension').value
-    console.log(Number.parseInt(ext))
-    e = ext
+    let e = document.querySelector('.extension').value
+    console.log(e)
 
-    // email manager
-    let emailManager = document.getElementById('emailManager').value.toLowerCase()
-    // location
-    let location = document.getElementById('location').value
+    const emailManager = document.getElementById('emailManager').value.toLowerCase()
+    console.log(emailManager)
+    const location = document.getElementById('location').value
+    console.log(location)    
+    const division = document.getElementById('division').value
+    console.log(division)
+    dominio_four = document.getElementById('dominio_four').value
+    console.log(dominio_four)
 
     let nome = document.querySelector('#name_id').value.toUpperCase().split('\n')
-    console.log(nome[1])
-    console.log(nome)
-
+    //Clear the indexes of array name
     nome = nome.filter(i => i.trim())
     console.log(nome)
 
     let login = document.getElementById('login_id').value.toLowerCase().split('\n')
     login = login.filter(z => z.trim())
     console.log(login)
-
-    i=0
-    var z=0
-    var hole = []
-    let string = `"name","email","phone_work","extension","title","department","email_manager","location_work","roles_divisions","queues","division","skills","hire_date"`
-    let division = document.getElementById('division').value
     
-    if(user == 0){
-        alert('Você não informou a quantidade de usuários!')
-    } else if(ext == 0){
-        alert('Você não informou a extension!')
-    } else if(division == 0){
-        alert('Você não selecionou alguma divisão!')
+    // Declaration of variables.
+    i=0
+    var hole = []
+    const string = `"name","email","phone_work","extension","title","department","email_manager","location_work","roles_divisions","queues","division","skills","hire_date"`
+    
+    if(division == 0){
+        alert('Você não selecionou alguma division!')
+        return
     } else if(nome.length != login.length){
         alert('A quantidade de nomes e e-mails estão diferentes!')
-    } else if(nome.length != user){
-        alert('Você informou a quantidade errada de usuários!')
-    }
+        return
+    } 
+      else if(e == 0){
+        const continuar = confirm('Você não informou a extension!\nClique em "OK" para continuar, ou "Cancelar" para sair!\nSe você continuar, os perfis irão ficar sem extension!')
+        console.log(continuar)
+        if(continuar === true && (nome.length && login.length) < 1){
+            alert('Você não adicionou dados nos campos, "Nomes" e "E-mails"!')
+            nomeStyle = document.getElementById('name_id')
+            loginStyle = document.getElementById('login_id')
+            nomeStyle.style.borderColor = 'red'
+            loginStyle.style.borderColor = 'red'
+            return
+        }
+        else if(continuar === true && (nome.length && login.length) >= 1){
+            nomeStyle.style.borderColor = 'black'
+            loginStyle.style.borderColor = 'black'
+            if (division == 1) {
+                console.log('1')
+                while(i<login.length){
+                    result = login[i].indexOf('@')
+                    if(result == -1){
+                        hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""`
+                    } else {
+                        hole[i] = `\n"${nome[i]}","${login[i++]}","","","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""`
+                    }
+                }
+            } else if (division == 2) {
+                console.log('2')
+                while(i<login.length){
+                    result = login[i].indexOf('@')
+                    if(result == -1){
+                        hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","","PRESTADOR","COORD GAO","${emailManager}","${location}","employee: [GAO], User: [GAO; Home]","","GAO","",""`
+                    } else{
+                        hole[i] = `\n"${nome[i]}","${login[i++]}","","","PRESTADOR","COORD GAO","${emailManager}","${location}","employee: [GAO], User: [GAO; Home]","","GAO","",""`
+                    }
+                }    
+            } else if (division == 3) {
+                console.log('3')
+                while(i<login.length){
+                    result = login[i].indexOf('@')
+                    if(result == -1){
+                        hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","","ANL OUVIDORIA JR","GER OUVIDORIA","${emailManager}","${location}","employee: [Contact Center; Ouvidoria], Communicate - User: [Contact Center; Ouvidoria], Outbound Agent: [Contact Center; Ouvidoria], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Ouvidoria]","","Ouvidoria","",""`
+                    } else{
+                        hole[i] = `\n"${nome[i]}","${login[i++]}","","","ANL OUVIDORIA JR","GER OUVIDORIA","${emailManager}","${location}","employee: [Contact Center; Ouvidoria], Communicate - User: [Contact Center; Ouvidoria], Outbound Agent: [Contact Center; Ouvidoria], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Ouvidoria]","","Ouvidoria","",""`
+                    }
+                }
+            } else if(division == 4){
+                console.log('4')
+                while(i<login.length){
+                    result = login[i].indexOf('@')
+                    if(result == -1){
+                        hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","","PRESTADOR","GER OPERACOES ASSIST E BENEFICIOS","${emailManager}","${location}","employee: [Tempo Assistência], User: [Home; Tempo Assistência]","","Tempo Assistência","",""`
+                    } else{
+                        hole[i] = `\n"${nome[i]}","${login[i++]}","","","PRESTADOR","GER OPERACOES ASSIST E BENEFICIOS","${emailManager}","${location}","employee: [Tempo Assistência], User: [Home; Tempo Assistência]","","Tempo Assistência","",""`
+                    }
+                } 
+            } else {
+                console.log('5')
+                while(i<login.length){
+                    result = login[i].indexOf('@')
+                    if(result == -1){
+                        hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","","PRESTADOR","","${emailManager}","${location}","employee: [Teste], Architect - Simple Access: [Teste]", , Teste, , `
+                    } else{
+                        hole[i] = `\n"${nome[i]}","${login[i++]}","","","PRESTADOR","","${emailManager}","${location}","employee: [Teste], Architect - Simple Access: [Teste]", , Teste, , `
+                    }
+                }
+            }
+        } else{
+            return
+        }
+    } else if(e !=0 && (nome.length && login.length) <= 0){
+        nomeStyle = document.getElementById('name_id')
+        loginStyle = document.getElementById('login_id')
+        alert('Você não adicionou dados nos campos, "Nomes" e "E-mails"!')
+        nomeStyle.style.borderColor = 'red'
+        loginStyle.style.borderColor = 'red'
+        return
+    } 
     else {
-       
+        nomeStyle = document.getElementById('name_id')
+        loginStyle = document.getElementById('login_id')
+        nomeStyle.style.borderColor = 'black'
+        loginStyle.style.borderColor = 'black'
         if (division == 1) {
             console.log('1')
-            while(i<user){
-                hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""`
+            while(i<login.length){
+                result = login[i].indexOf('@')
+                if(result == -1){
+                    hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","${e++}","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""`
+                } else{
+                    hole[i] = `\n"${nome[i]}","${login[i++]}","","${e++}","ATENDENTE","SUPV RELAC COM CLIENTES I","${emailManager}","${location}","employee: [Contact Center], Communicate - User: [Contact Center], Outbound Agent: [Contact Center], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Home]","","Contact Center","",""`
+                }
             }
         } else if (division == 2) {
             console.log('2')
-            while(i<user){
-                hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","PRESTADOR","COORD GAO","${emailManager}","${location}","employee: [GAO], User: [GAO; Home]","","GAO","",""`
+            while(i<login.length){
+                result = login[i].indexOf('@')
+                if(result == -1){
+                    hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","${e++}","PRESTADOR","COORD GAO","${emailManager}","${location}","employee: [GAO], User: [GAO; Home]","","GAO","",""`
+                } else{
+                    hole[i] = `\n"${nome[i]}","${login[i++]}","","${e++}","PRESTADOR","COORD GAO","${emailManager}","${location}","employee: [GAO], User: [GAO; Home]","","GAO","",""`
+                }
             }    
         } else if (division == 3) {
             console.log('3')
-            while(i<user){
-                hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","ANL OUVIDORIA JR","GER OUVIDORIA","${emailManager}","${location}","employee: [Contact Center; Ouvidoria], Communicate - User: [Contact Center; Ouvidoria], Outbound Agent: [Contact Center; Ouvidoria], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Ouvidoria]","","Ouvidoria","",""`
+            while(i<login.length){
+                result = login[i].indexOf('@')
+                if(result == -1){
+                    hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","${e++}","ANL OUVIDORIA JR","GER OUVIDORIA","${emailManager}","${location}","employee: [Contact Center; Ouvidoria], Communicate - User: [Contact Center; Ouvidoria], Outbound Agent: [Contact Center; Ouvidoria], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Ouvidoria]","","Ouvidoria","",""`
+                } else{
+                    hole[i] = `\n"${nome[i]}","${login[i++]}","","${e++}","ANL OUVIDORIA JR","GER OUVIDORIA","${emailManager}","${location}","employee: [Contact Center; Ouvidoria], Communicate - User: [Contact Center; Ouvidoria], Outbound Agent: [Contact Center; Ouvidoria], Permissões Adicionais TLV Agile: [Broto; Contact Center; Default; GAO; Home; Mapfre; Núcleo Sinistro Multirrisco; Ouvidoria; Tempo Assistência; Tecnologia; Telesubscrição], User: [Contact Center; Ouvidoria]","","Ouvidoria","",""`
+                }
             }
         } else if(division == 4){
             console.log('4')
-            while(i<user){
-                hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","PRESTADOR","GER OPERACOES ASSIST E BENEFICIOS","${emailManager}","${location}","employee: [Tempo Assistência], User: [Home; Tempo Assistência]","","Tempo Assistência","",""`
+            while(i<login.length){
+                result = login[i].indexOf('@')
+                if(result == -1){
+                    hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","${e++}","PRESTADOR","GER OPERACOES ASSIST E BENEFICIOS","${emailManager}","${location}","employee: [Tempo Assistência], User: [Home; Tempo Assistência]","","Tempo Assistência","",""`
+                } else{
+                    hole[i] = `\n"${nome[i]}","${login[i++]}","","${e++}","PRESTADOR","GER OPERACOES ASSIST E BENEFICIOS","${emailManager}","${location}","employee: [Tempo Assistência], User: [Home; Tempo Assistência]","","Tempo Assistência","",""`
+                }
             } 
         } else {
             console.log('5')
-            while(i<user){
-                hole[i] = `\n"${nome[i++]}","${login[z++]}","","${e++}","PRESTADOR","","${emailManager}","${location}","employee: [Teste], Architect - Simple Access: [Teste]", , Teste, , `
+            while(i<login.length){
+                result = login[i].indexOf('@')
+                if(result == -1){
+                    hole[i] = `\n"${nome[i]}","${login[i++]}${dominio_four}","","${e++}","PRESTADOR","","${emailManager}","${location}","employee: [Teste], Architect - Simple Access: [Teste]", , Teste, , `
+                } else{
+                    hole[i] = `\n"${nome[i]}","${login[i++]}","","${e++}","PRESTADOR","","${emailManager}","${location}","employee: [Teste], Architect - Simple Access: [Teste]", , Teste, , `
+                }
             }
         } 
-        var myBlob = new Blob(
-            [string, hole.join("")],
-            {type: "text/plain"}
-            )
-            
-            // File Handler $ STREAM
-            const fileHandle = await window.showSaveFilePicker()
-            const fileStream = await fileHandle.createWritable()
-            
-            // SAVE FILE
-            await fileStream.write(myBlob)
-            await fileStream.close()
     }
+    var myBlob = new Blob(
+        [string, hole.join("")],
+        {type: "text/plain"}
+        )
+        
+        // File Handler $ STREAM
+        const fileHandle = await window.showSaveFilePicker()
+        const fileStream = await fileHandle.createWritable()
+        
+        // SAVE FILE
+        await fileStream.write(myBlob)
+        await fileStream.close()
 }
 
-let body = document.querySelector('body')
+// #Back variables
 const back = document.getElementById('back')
 const front = document.getElementById('front')
 const card = document.getElementById('card')
 const four = document.querySelector('#four')
-back.style.visibility = 'hidden'
+const quatro = document.getElementById('quatro')
 
+const checkbox = document.querySelector('#checkbox')
+let userWebRTC = document.getElementById('userWebRTC')
+let loginWebRTC_id = document.getElementById('loginWebRTC_id')
+
+// Flip to the WebRTC, via the button
 function WebRTC(){
-    back.style.visibility = 'visible'
     card.classList.toggle('flip')
 }
-
+// Flip to the WebRTC, via the h1 -> #four
 four.addEventListener('click', ()=>{
-    back.style.visibility = 'visible'
     card.classList.toggle('flip')
 })
-
-const quatro = document.getElementById('quatro')
+// Flip to the WebRTC, via the h1 -> #quatro
 quatro.addEventListener('click', ()=>{
     card.classList.toggle('flip')
 })
 
-const checkbox = document.querySelector('#checkbox')
-var userWebRTC = document.getElementById('userWebRTC')
-var loginWebRTC_id = document.getElementById('loginWebRTC_id')
 
 function viewcheckbox(){
     if(checkbox.checked === true){
         loginWebRTC_id.disabled = true
+        loginWebRTC_id.style.backgroundColor = 'rgb(182, 182, 182)'
     } else {
         loginWebRTC_id.disabled = false
+        loginWebRTC_id.style.backgroundColor = 'white'
     }
 }
 
